a20daf524b8a2a92e96d4aec7711ee48f3cf3a0a
Isadora Bassetto Kwiatkowski <isadorabk@gmail.com>
2018-06-30 15:38:09 +0200

    koa server installed

--- server/index.js
+++ server/index.js
@@ -4,22 +4,18 @@ const Koa = require('koa');
 const app = new Koa();
 const serve = require('koa-static');
 const bodyParser = require('koa-bodyparser');
+const socket = require('socket.io');
 
 const config = require('./config.js');
-const router = require('./router.js');
+// const router = require('./router.js');
 
 const port = 3000;
 
 app
   .use(serve(config.clientPath))
   .use(bodyParser())
-  .use(router.routes())
-  .listen(port);
-
-console.log(`Server listening on port ${port}.`);
-
-
-
-
-
-
+  // .use(router.routes())
+  .listen(port, (err) => {
+    if (err) console.log(`Error connecting the server: ${err}`);
+    else console.log(`Server listening on port ${port}.`);
+  });
